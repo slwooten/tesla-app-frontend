e9f268f823cb324bf34f5b10d4aa262a5a503c21
Seth Wooten <slwooten14@gmail.com>
2022-07-21 20:12:44 -0400

    button component completed

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import { View, Text, ImageBackground } from 'react-native';
 import styles from './styles';
+import StyledButton from '../StyledButton';
 
 const CarItem = (props) => {
   return (
     <View style={styles.carContainer}>
-        <ImageBackground source={require('../../assets/images/ModelS.jpeg')} style={styles.image} />
-        <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subtitle}>Starting at $69,420</Text>
-        </View>
+      <ImageBackground source={require('../../assets/images/ModelS.jpeg')} style={styles.image} />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+
+      <StyledButton
+        type='primary'
+        content={'Custom Order'}
+        onPress={() => {
+          console.log('Custom Order was Pressed.');
+        }}
+      />
+
+      <StyledButton
+        type='secondary'
+        content={'Existing Inventory'}
+        onPress={() => {
+          console.log('Existing Inventory was Pressed.');
+        }}
+      />
+
+    </View>
   );
 };
 
