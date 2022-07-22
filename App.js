830e38e4810c157d33de44bbecfac605cbe89de6
Seth Wooten <slwooten14@gmail.com>
2022-07-21 20:26:10 -0400

    car component complete

--- App.js
+++ App.js
@@ -6,7 +6,14 @@ import CarItem from './components/CarItem';
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem />
+
+      <CarItem
+        name={'Model X'}
+        tagLine={'Order Online For'}
+        tagLineCTA={'Touchless Delivery'}
+        image={require('./assets/images/ModelX.jpeg')}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -4,29 +4,40 @@ import styles from './styles';
 import StyledButton from '../StyledButton';
 
 const CarItem = (props) => {
+
+  const { name, tagLine, tagLineCTA, image } = props;
+
   return (
     <View style={styles.carContainer}>
-      <ImageBackground source={require('../../assets/images/ModelS.jpeg')} style={styles.image} />
+      <ImageBackground
+        source={image}
+        style={styles.image}
+      />
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>{tagLine} {' '}
+          <Text style={styles.subtitleCTA}>{tagLineCTA}</Text>
+        </Text>
+
       </View>
 
-      <StyledButton
-        type='primary'
-        content={'Custom Order'}
-        onPress={() => {
-          console.log('Custom Order was Pressed.');
-        }}
-      />
+      <View style={styles.buttonContainer}>
+        <StyledButton
+          type='primary'
+          content={'Custom Order'}
+          onPress={() => {
+            console.log('Custom Order was Pressed.');
+          }}
+        />
 
-      <StyledButton
-        type='secondary'
-        content={'Existing Inventory'}
-        onPress={() => {
-          console.log('Existing Inventory was Pressed.');
-        }}
-      />
+        <StyledButton
+          type='secondary'
+          content={'Existing Inventory'}
+          onPress={() => {
+            console.log('Existing Inventory was Pressed.');
+          }}
+        />
+      </View>
 
     </View>
   );
